5be589a8bd4d880738cf53098de4879f797c23ae
Ross <github@hypn.za.net>
2013-03-17 15:12:29 +0200

    listen on port 80 of all interfaces, and talk to PiFace

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
-# import piface.pfio as pfio
-# pfio.init()
+import piface.pfio as pfio
+pfio.init()
 
 from flask import Flask, render_template
 from werkzeug import SharedDataMiddleware
@@ -18,15 +18,15 @@ def home():
 
 @app.route('/on/<pin_id>')
 def on(pin_id):
-  # global pfio
-  # pfio.digital_write(pin_id, 1)
+  global pfio
+  pfio.digital_write(int(pin_id), 1)
   return 'Turning pin ' + pin_id + ' ON'
 
 @app.route('/off/<pin_id>')
 def off(pin_id):
-  # global pfio
-  # pfio.digital_write(pin_id, 0)
+  global pfio
+  pfio.digital_write(int(pin_id), 0)
   return 'Turning pin ' + pin_id + ' OFF'
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run('0.0.0.0', 80)
